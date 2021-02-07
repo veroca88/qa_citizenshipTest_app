e7cc8cc6dfa280e7d052113048540b63704d8222
veroca88 <veronicadonoso1506@gmail.com>
2021-02-07 14:07:21 -0600

    Create Question box that receives props and method from App.vue

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="qbox-container">
+    <b-jumbotron>
+      <template slot="lead">{{ currentQuestion.question }}</template>
+
+      <hr class="my-4" />
+
+      <b-list-group>
+        <b-list-group-item v-for="(answer, index) in answers" :key="index">{{
+          answer
+        }}</b-list-group-item>
+      </b-list-group>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+  padding: 0% 5%;
+}
+</style>
