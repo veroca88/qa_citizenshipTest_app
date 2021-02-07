22aa56b055d10cf8d146d239f9acdc0874f9f1dd
veroca88 <veronicadonoso1506@gmail.com>
2021-02-07 14:05:41 -0600

    Create component header of the app

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <b-nav tabs align="center">
+      <b-nav-item disabled><b>Citizenship Quiz App</b></b-nav-item>
+      <i class="fas fa-flag-usa fa-2x"></i>
+      <b-nav-item disabled>Counter: 4/10</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<style>
+</style>
