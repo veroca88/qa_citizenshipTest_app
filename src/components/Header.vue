cb1ea2ec2fff3e0f2370e81c6f243a5cffd60a8c
veroca88 <veronicadonoso1506@gmail.com>
2021-02-07 19:09:43 -0600

    Pass props from App to Header component and use in score counter

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,10 +3,16 @@
     <b-nav tabs align="center">
       <b-nav-item disabled><b>Citizenship Quiz App</b></b-nav-item>
       <i class="fas fa-flag-usa fa-2x"></i>
-      <b-nav-item disabled>Counter: 4/10</b-nav-item>
+      <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
     </b-nav>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["numCorrect", "numTotal"],
+};
+</script>
 
 <style>
 </style>
